928d4a83e2068e98da708f490ce9e13e836fa8fd
Nada Soliman <nada.mohammad7654@gmail.com>
2022-12-26 16:25:53 +0200

    check updates in script Create DB

--- shell-project.sh
+++ shell-project.sh
@@ -1,30 +1,77 @@
 #!/usr/bin/bash
 
-echo "welcome to our DBMS"
+# echo "welcome to our DBMS"
 
+# echo "Your folder DataBases is created in your current path"
 
 export LC_COLLATE=C;
 shopt -s extglob;
 
+if [ -d "DBMS" ] ;then    # ./
+echo "welcome to our DBMS"
+cd DBMS;
+else
+echo "welcome first time"
+mkdir ./DBMS;
+cd DBMS;
+fi
+echo -n "your path now is " ;pwd
+
+# we are in DBMS folder
+echo "you can create , list ,...... DB"
 
 select choice in createDB listDB connectToDB dropDB exit
 do
 case $choice in
-"createDB")
-    echo "creat pressed"
-    read -p "enter name :" name
-    echo "DB created"
+createDB)
+
+    # echo "creat pressed"
+    read -p "enter name of database:" DBname
+
+# check regex
+reg=[^[a-z][A-Z]_]*];
+if [[ $DBname =~ $reg ]] ;then
+    echo "valid"
+    ls
+    # check exist 
+
+    if [ -d $DBname ] ;then    # ./
+        echo "welcome back to your database " "named " $DBname ":)"
+        cd $DBname;
+        pwd
+    else
+        echo "welcome first time"
+        mkdir ./$DBname;
+        ls
+        cd $DBname
+    fi
+else
+echo "out"
+
+
+
+
+
+
+
+
+
+
+fi
+   echo " *** *** ***outside regex  choose 1 2 3 4 5"
+ 
 ;;
-"listDB")
+listDB)
     echo "list pressed"
+    ls 
 ;;
-"connectToDB")
+connectToDB)
     echo "connect pressed"
 ;;
-"dropDB")
+dropDB)
     echo "drop pressed"
 ;;
-"exit")
+exit)
     break
 ;;
 *)
